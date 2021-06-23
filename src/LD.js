1589bff2a1ae789c25401ab75403fc2800b53563
Phil Zona <pzona@launchdarkly.comm>
2021-06-23 16:42:55 -0500

    Remove token again

--- src/LD.js
+++ src/LD.js
@@ -3,7 +3,7 @@ const LaunchDarklyApi = require('launchdarkly-api');
 // Configure the client
 const LDClient = LaunchDarklyApi.ApiClient.instance;
 const Token = LDClient.authentications['Token'];
-Token.apiKey = 'api-df97d0aa-2fb7-4ad3-863f-e1ded44abf54';
+Token.apiKey = '';
 
 // Retrieve configuration options
 const projectKey = process.env.LD_PROJECT_KEY;
